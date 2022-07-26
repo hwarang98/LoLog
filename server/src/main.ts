f581efa61aba83061fcf0ab977e8ea7aa6f52591
LeeHwaRang <snow2271@gmail.com>
2022-07-26 22:33:37 +0900

    client cors setting

--- server/src/main.ts
+++ server/src/main.ts
@@ -21,6 +21,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
+  app.enableCors();
   await app.listen(4000);
 }
 bootstrap();
