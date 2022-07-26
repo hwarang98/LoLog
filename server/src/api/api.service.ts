01da83d5811fffd14e34a8021885ce9198529548
LeeHwaRang <snow2271@gmail.com>
2022-07-26 22:34:59 +0900

    라이엇 api  유저정보 get 요청

--- server/src/api/api.service.ts
+++ server/src/api/api.service.ts
@@ -1,28 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
+import { UserInfo } from './interface/user.interface';
+import { AxiosResponse } from 'axios';
 
 @Injectable()
 export class ApiService {
   constructor(private readonly httpService: HttpService) {}
+  async getUserInfo(name: string): Promise<AxiosResponse<UserInfo>> {
+    console.log(`name is ${name}`);
 
-  async getUserInfo(name: string): Promise<any> {
     const url: string = 'https://kr.api.riotgames.com';
-    console.log('유저 정보 요청');
-    console.log('닉네임: ', name);
-    const result = await this.httpService.get(
-      `${url}/lol/summoner/v4/summoners/by-name/${encodeURI(name)}?api_key=${
-        process.env.RIOT_API_KEY
-      }`,
-      {
-        headers: {
-          'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
-          'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
-          Origin: 'https://developer.riotgames.com',
-          'X-Riot-Token': process.env.RIOT_API_KEY,
+    return await this.httpService.axiosRef
+      .get(
+        `${url}/lol/summoner/v4/summoners/by-name/${encodeURI(name)}?api_key=${
+          process.env.RIOT_API_KEY
+        }`,
+        {
+          headers: {
+            'X-Riot-Token': process.env.RIOT_API_KEY,
+          },
         },
-      },
-    );
-    // .toPromise();
-    return result;
+      )
+      .then((data) => {
+        return data.data;
+      });
   }
 }
