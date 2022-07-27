38785a4189758d089be668c9e65275ca87f5f0b3
LeeHwaRang <snow2271@gmail.com>
2022-07-27 22:25:03 +0900

    라이엇 api에 matchId 최근 10게임 요청
    matchId 로 하나의 게임 정보 상세 요청 가능

--- server/src/api/api.service.ts
+++ server/src/api/api.service.ts
@@ -1,28 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { UserInfo } from './interface/user.interface';
 import { AxiosResponse } from 'axios';
+// import { UserInfo } from './interface/user.interface';
 
 @Injectable()
 export class ApiService {
   constructor(private readonly httpService: HttpService) {}
-  async getUserInfo(name: string): Promise<AxiosResponse<UserInfo>> {
-    console.log(`name is ${name}`);
 
+  // 유저 정보 요청
+  async getUserInfo(name: string): Promise<AxiosResponse> {
     const url: string = 'https://kr.api.riotgames.com';
-    return await this.httpService.axiosRef
-      .get(
-        `${url}/lol/summoner/v4/summoners/by-name/${encodeURI(name)}?api_key=${
-          process.env.RIOT_API_KEY
-        }`,
-        {
-          headers: {
-            'X-Riot-Token': process.env.RIOT_API_KEY,
-          },
+    let userData = await this.httpService.axiosRef.get(
+      `${url}/lol/summoner/v4/summoners/by-name/${encodeURI(name)}?api_key=${
+        process.env.RIOT_API_KEY
+      }`,
+      {
+        headers: {
+          'X-Riot-Token': process.env.RIOT_API_KEY,
         },
-      )
-      .then((data) => {
-        return data.data;
-      });
+      },
+    );
+    return userData.data;
+  }
+
+  // 게임 매칭 id 요청
+  async matchId(puuid: string): Promise<AxiosResponse> {
+    const url: string = 'https://asia.api.riotgames.com';
+    let matchData = await this.httpService.axiosRef.get(
+      `${url}/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20
+      `,
+      {
+        headers: {
+          'X-Riot-Token': process.env.RIOT_API_KEY,
+        },
+        withCredentials: true,
+      },
+    );
+    return matchData.data;
   }
 }
