227fe5d72e5002f549863051435455b512c58c11
LeeHwaRang <snow2271@gmail.com>
2022-07-28 23:31:39 +0900

    게임정보 요청 구현

--- server/src/api/api.service.ts
+++ server/src/api/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { AxiosResponse } from 'axios';
-// import { UserInfo } from './interface/user.interface';
+import { UserInfo } from './interface/user.interface';
 
 @Injectable()
 export class ApiService {
@@ -33,9 +33,24 @@ export class ApiService {
         headers: {
           'X-Riot-Token': process.env.RIOT_API_KEY,
         },
-        withCredentials: true,
       },
     );
     return matchData.data;
+  }
+
+  async gameInfo(matchId: string[]): Promise<AxiosResponse> {
+    const url: string = 'https://asia.api.riotgames.com';
+    for (let i = 0; i < matchId.length; i++) {
+      let gameInfo = await this.httpService.axiosRef.get(
+        `${url}/lol/match/v5/matches/${matchId[i]}
+        `,
+        {
+          headers: {
+            'X-Riot-Token': process.env.RIOT_API_KEY,
+          },
+        },
+      );
+      return gameInfo.data;
+    }
   }
 }
