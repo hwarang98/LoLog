d808cc9f0fabf0d35f537c76b0d712bd116187f3
LeeHwaRang <snow2271@gmail.com>
2022-07-28 23:32:09 +0900

    server에 matchId 전송

--- client/src/pages/Summoner.js
+++ client/src/pages/Summoner.js
@@ -1,27 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import "./summoner.css";
 
 function Summoner() {
   const [matchId, setMatchId] = useState([]);
-
+  const [gameInfo, setGetGameInfo] = useState([]);
   const { state } = useLocation();
   const userInfo = state.data;
   const name = state.name;
   const puuid = state.puuid;
 
-  // matchId request
+  // matchId 조회 요청
   useEffect(() => {
-    const matchData = async () => {
-      let response = await axios.post("http://localhost:4000/api/matchInfo", {
+    const getMatchData = async () => {
+      let getMatchId = await axios.post("http://localhost:4000/api/matchInfo", {
         puuid: puuid,
       });
-      setMatchId(response.data);
-      return response;
+      return setMatchId(getMatchId.data);
+      // return getMatchId;
     };
-    matchData();
+    getMatchData();
   }, []);
+
+  // matchId에 해당하는 게임 정보조회 요청
+  const getGameInfo = async () => {
+    console.log(matchId);
+    let getGameInfo = await axios.post("http://localhost:4000/api/gameInfo", {
+      matchId: matchId,
+    });
+    console.log(getGameInfo);
+    setGetGameInfo(getGameInfo.data);
+    return getGameInfo;
+  };
+  console.log(gameInfo);
+
   return (
     <div className="summoner">
       <div className="profile">
@@ -32,6 +45,7 @@ function Summoner() {
       </div>
       <p></p>
       <ul>
+        <button onClick={getGameInfo}>전적검색</button>
         {matchId.map((item, index) => {
           return <li key={index}>{item}</li>;
         })}
