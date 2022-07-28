3487781d4be2ab538a55793f3f28e6731a10eee8
LeeHwaRang <snow2271@gmail.com>
2022-07-28 23:31:49 +0900

    게임정보 요청 구현

--- server/src/api/api.controller.ts
+++ server/src/api/api.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { ApiService } from './api.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 @ApiTags('riot api')
 @Controller('api')
@@ -8,12 +8,31 @@ export class ApiController {
   constructor(private ApiService: ApiService) {}
 
   @Post('userInfo')
-  getMatch(@Body('name') name: string) {
+  @ApiOperation({
+    summary: '유저 정보 조회 API',
+    description: '소환사 이름을 사용한 해당 유저 정보 조회',
+  })
+  getUser(@Body('name') name: string) {
     return this.ApiService.getUserInfo(name);
   }
 
   @Post('matchInfo')
-  getUser(@Body('puuid') puuid: string) {
+  @ApiOperation({
+    summary: '게임 matchId 조회 API',
+    description:
+      'userInfo API 에서 요청받은 항목중 puuid를 사용한 게임 매칭 ID조회',
+  })
+  getMatch(@Body('puuid') puuid: string) {
     return this.ApiService.matchId(puuid);
+  }
+
+  @Post('gameInfo')
+  @ApiOperation({
+    summary: '게임정보 조회 API',
+    description: 'matchInfo API에서 요청한 matchId를 사용해 해당 게임 정보조회',
+  })
+  getGameInfo(@Body('matchId') matchId: string[]) {
+    console.log('matchId: ', matchId);
+    return this.ApiService.gameInfo(matchId);
   }
 }
