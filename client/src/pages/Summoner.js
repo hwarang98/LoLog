aaae06710a16e567984d0e3a739944ec6d47a5c7
LeeHwaRang <snow2271@gmail.com>
2022-07-31 20:42:34 +0900

    페이지 컴포넌트

--- client/src/pages/Summoner.js
+++ client/src/pages/Summoner.js
@@ -1,13 +1,21 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useLocation } from "react-router-dom";
+import { Profile, SoloGameRankRate } from "../components";
 import axios from "axios";
 import "./summoner.css";
 
 function Summoner() {
   const [matchId, setMatchId] = useState([]);
   const [gameInfo, setGetGameInfo] = useState([]);
+  const [soloRankInfo, setSoloRankInfo] = useState([]);
+  const [freeRankInfo, setFreeRankInfo] = useState([]);
   const { state } = useLocation();
-  const { name, puuid, profileIconId, summonerLevel } = state;
+  const { name, puuid, profileIconId, summonerLevel } = state.userData;
+
+  useEffect(() => {
+    setSoloRankInfo(state.leagueData[0]);
+    setFreeRankInfo(state.leagueData[1]);
+  }, []);
 
   // matchId 조회 요청
   useEffect(() => {
@@ -26,22 +34,20 @@ function Summoner() {
     let getGameInfo = await axios.post("http://localhost:4000/api/gameInfo", {
       matchId: matchId,
     });
-    // console.log("getGameInfo: ", getGameInfo);
     setGetGameInfo(getGameInfo.data);
     return getGameInfo;
   };
 
-  console.log("gameInfo: ", gameInfo);
+  // console.log("gameInfo: ", gameInfo);
 
   return (
     <div className="summoner">
-      <div className="profile">
-        {/* <img src="example" alt="유저 아이콘" /> */}
-        <div className="name">{name}</div>
-        <div className="level">{summonerLevel}</div>
-        <div>최근 20 게임 승률: 75%</div>
-      </div>
-      <p></p>
+      <Profile
+        name={name}
+        level={summonerLevel}
+        leagueType={soloRankInfo.queueType}
+      />
+      <SoloGameRankRate wins={soloRankInfo.wins} losses={soloRankInfo.losses} />
       <ul>
         <button onClick={getGameInfo}>전적검색</button>
         <ul>
