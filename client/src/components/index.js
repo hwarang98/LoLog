30e90fb8e8bfa02baceffac85c9ec43fdca42015
LeeHwaRang <snow2271@gmail.com>
2022-07-31 20:41:27 +0900

    컴포넌트 미들웨어 생성

---
+++ client/src/components/index.js
@@ -0,0 +1,4 @@
+// middleware
+export { default as Profile } from "./Profile";
+export { default as SoloGameRankRate } from "./SoloGameRankRate";
+export { default as FreeRankGameRate } from "./FreeGameRankRate";
