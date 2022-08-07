170945852d11d9a6f50581320dac73f27720be8d
LeeHwaRang <snow2271@gmail.com>
2022-08-07 21:48:57 +0900

    post로 넘어온 데이터 추가

--- server/src/api/api.controller.ts
+++ server/src/api/api.controller.ts
@@ -22,7 +22,6 @@ export class ApiController {
     description: '암호화된 소환사 아이디를 사용한 해당 소환사 리그정보 조회',
   })
   getLeague(@Body('cryptoId') cryptoId: string) {
-    console.log(cryptoId);
     return this.ApiService.getLeagueInfo(cryptoId);
   }
 
@@ -41,8 +40,8 @@ export class ApiController {
     summary: '게임정보 조회 API',
     description: 'matchInfo API에서 요청한 matchId를 사용해 해당 게임 정보조회',
   })
-  getGameInfo(@Body('matchId') matchId: string[]) {
-    console.log('matchId: ', matchId);
-    return this.ApiService.gameInfo(matchId);
+  getGameInfo(@Body('matchId') matchId: string[], @Body('name') name: string) {
+    const data: any[] = [matchId, name];
+    return this.ApiService.gameInfo(data);
   }
 }
