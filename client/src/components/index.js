f3c8b4a0a8725d0019480bde7c00204b488ddc8c
LeeHwaRang <snow2271@gmail.com>
2022-08-07 21:50:13 +0900

    컴포넌트 middleware 추가

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,3 +2,4 @@
 export { default as Profile } from "./Profile";
 export { default as SoloGameRankRate } from "./SoloGameRankRate";
 export { default as FreeRankGameRate } from "./FreeGameRankRate";
+export { default as GameInfoRender } from "./GameInfoRender";
