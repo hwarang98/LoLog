a5d3e091823fbf6640870a9833507443b757b3ec
LeeHwaRang <snow2271@gmail.com>
2022-08-12 18:43:20 +0900

    게임 정보 렌더링

--- client/src/components/GameInfoRender.js
+++ client/src/components/GameInfoRender.js
@@ -1,8 +1,27 @@
 import React from "react";
+import axios from "axios";
 
 function GameInfoRender(props) {
   const { gameInfo } = props;
 
+  const championIcon = async () => {
+    const getIcon = await axios.get(
+      "http://ddragon.leagueoflegends.com/cdn/12.15.1/data/en_US/champion.json"
+    );
+    console.log();
+  };
+
+  // 2번째 소숫점 자리부터 버리는 함수
+  const getNotRoundDecimalNumber = (number, decimalPoint = 2) => {
+    let num = typeof number === "number" ? String(number) : number;
+    const pointPos = num.indexOf(".");
+
+    if (pointPos === -1) return Number(num).toFixed(decimalPoint);
+
+    const splitNumber = num.split(".");
+    const rightNum = splitNumber[1].substring(0, decimalPoint);
+    return Number(`${splitNumber[0]}.${rightNum}`).toFixed(decimalPoint);
+  };
   // teamId 200 = 레드팀
   // teamId 100 = 블루팀
   return (
@@ -10,32 +29,34 @@ function GameInfoRender(props) {
       {gameInfo.map((item, index) => {
         console.log("item: ", item);
         return (
-          <ul key={index}>
-            <li>챔피언 이름: {item.championName}</li>
-            <li>레벨: {item.champLevel}</li>
-            <li>챔피언 ID: {item.championId}</li>
-            <li>
-              {item.kills}/{item.deaths}/{item.assists}{" "}
-              {item.deaths === 0 ? "Perfect" : `(${item.kda})`}
+          <div className="gameInfo" key={index}>
+            <li className="gameList">
+              챔피언 이름: {item.championName}
+              레벨: {item.champLevel}
+              챔피언 ID: {item.championId}
+              {item.kills}/{item.deaths}/{item.assists}
+              {item.deaths === 0
+                ? "Perfect"
+                : `(${getNotRoundDecimalNumber(item.kda)})`}
+              총 딜량: {item.totalDamageDealtToChampions}
+              받은 피해량: {item.totalDamageTaken}
+              솔킬 횟수: {item.soloKills}
+              퍼스트 블러드: {item.firstBloodKill}
+              더블킬 횟수: {item.doubleKills}
+              트리플킬 횟수: {item.tripleKills}
+              팬타킬 횟수: {item.pentaKills}
+              아이탬1: {item.item0}
+              아이탬2: {item.item1}
+              아이탬3: {item.item2}
+              아이탬4: {item.item3}
+              아이탬5: {item.item4}
+              아이탬6: {item.item5}
+              아이탬7: {item.item6}
+              {item.teamId === 200 ? "레드팀" : "블루팀"}
+              라인: {item.teamPosition}
+              {item.win === true ? "승리" : "패배"}
             </li>
-            <li>총 딜량: {item.totalDamageDealtToChampions}</li>
-            <li>받은 피해량: {item.totalDamageTaken}</li>
-            <li>솔킬 횟수: {item.soloKills}</li>
-            <li>퍼스트 블러드: {item.firstBloodKill}</li>
-            <li>더블킬 횟수: {item.doubleKills}</li>
-            <li>트리플킬 횟수: {item.tripleKills}</li>
-            <li>팬타킬 횟수: {item.pentaKills}</li>
-            <li>아이탬1: {item.item0}</li>
-            <li>아이탬2: {item.item1}</li>
-            <li>아이탬3: {item.item2}</li>
-            <li>아이탬4: {item.item3}</li>
-            <li>아이탬5: {item.item4}</li>
-            <li>아이탬6: {item.item5}</li>
-            <li>아이탬7: {item.item6}</li>
-            <li>{item.teamId === 200 ? "레드팀" : "블루팀"}</li>
-            <li>라인: {item.teamPosition}</li>
-            <li>{item.win === true ? "승리" : "패배"}</li>
-          </ul>
+          </div>
         );
       })}
     </div>
