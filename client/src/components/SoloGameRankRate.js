87264765299822bcc2d1df7feed105b5964ca918
LeeHwaRang <snow2271@gmail.com>
2022-08-17 21:22:04 +0900

    코드정리 (기능변경 x)

--- client/src/components/SoloGameRankRate.js
+++ client/src/components/SoloGameRankRate.js
@@ -1,14 +1,19 @@
 import React from "react";
 
 function SoloGameRate(props) {
-  const { wins, losses } = props;
+  const { wins, losses, winCount } = props;
+
+  const allGameWinRate = (winCount / 10) * 100;
+
   // 솔로랭크 승률
   const soloRankGameSum = wins + losses;
   const soloRankGameRate = Math.ceil((wins / soloRankGameSum) * 100);
   return (
     <div className="gameRateInfo">
-      승률: {soloRankGameRate}%({wins}승 {losses}패)
-      <div>최근 20 게임 승률: 75%</div>
+      <div className="soloRankGameInfo">
+        승률: {soloRankGameRate}%({wins}승 {losses}패)
+      </div>
+      <div className="recentGames">최근 10 게임 승률: {allGameWinRate}%</div>
     </div>
   );
 }
