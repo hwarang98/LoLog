2a1c6b16e245ed33b80e4c4bfc314668a589d52c
LeeHwaRang <snow2271@gmail.com>
2022-08-17 21:21:26 +0900

    소환사 티어 엠블램 컴포넌트 미들웨어 등록

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,3 +3,4 @@ export { default as Profile } from "./Profile";
 export { default as SoloGameRankRate } from "./SoloGameRankRate";
 export { default as FreeRankGameRate } from "./FreeGameRankRate";
 export { default as GameInfoRender } from "./GameInfoRender";
+export { default as Emblem } from "./Emblem";
