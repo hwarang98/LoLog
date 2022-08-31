3ce5f5dd71aea73a9db4810c3a7dfbd7b5388dd1
LeeHwaRang <snow2271@gmail.com>
2022-08-31 22:34:23 +0900

    스키마 모듈 등록

--- server/src/api/api.module.ts
+++ server/src/api/api.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
 import { HttpModule } from '@nestjs/axios';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Summoner, SummonerSchema } from './schema/summoner.schema';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    MongooseModule.forFeature(
+      [{ name: Summoner.name, schema: SummonerSchema }],
+      // 'summoner',
+    ),
+  ],
   controllers: [ApiController],
   providers: [ApiService],
 })
