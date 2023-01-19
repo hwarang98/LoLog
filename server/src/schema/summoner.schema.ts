69df170bd8b2fa42a8695cd8a18945613bb60297
hwarnag <snow2271@gmail.com>
2023-01-19 16:17:14 +0900

    버전키 삭제, 오타수정

--- server/src/schema/summoner.schema.ts
+++ server/src/schema/summoner.schema.ts
@@ -1,14 +1,14 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
-import { Document, Mixed, ObjectExpression, ObjectId } from 'mongoose';
+import { Document, Mixed, ObjectExpression, ObjectId, version } from 'mongoose';
 
 export type SummonerDocument = Summoner & Document;
 
-@Schema()
+@Schema({ versionKey: false })
 export class Summoner {
   @ApiProperty({
     example: '돌면킬',
-    description: '소화사 이름',
+    description: '소환사 이름',
   })
   @Prop({ required: true })
   summonerName: string;
