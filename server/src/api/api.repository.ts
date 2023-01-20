65d64700941f5472e01c9b83a3ad8624d276a40b
hwarnag <snow2271@gmail.com>
2023-01-20 14:55:14 +0900

    메서드 설명 주석 추가

--- server/src/api/api.repository.ts
+++ server/src/api/api.repository.ts
@@ -13,7 +13,7 @@ export class SummonerRepository {
   /**
    *
    * @param data dto를 거친 api data
-   * @설명 api rawData를 몽고DB애 저장하는 함수
+   * @설명 게임정보 저장
    */
   async gameInfoSave(data: SummonerData) {
     try {
@@ -26,6 +26,11 @@ export class SummonerRepository {
     }
   }
 
+  /**
+   *
+   * @param name string 문자열
+   * @returns 해당 유저의 게임 정보
+   */
   async getGameData(name: SummonerData) {
     try {
       const summonerName = name.summonerName;
@@ -47,9 +52,5 @@ export class SummonerRepository {
     } catch (error) {
       throw new HttpException('소환사가 이미 존재합니다.', 400);
     }
-  }
-
-  async getGameSaveLog(summonerName: string) {
-    return this.summonerModel.find({ summonerName: summonerName }, { _id: 1, summonerName: 1 });
   }
 }
