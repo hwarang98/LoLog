eff916df3fbe62e0274c75913edb01304f90d65f
LeeHwaRang <snow2271@gmail.com>
2023-02-12 17:35:06 +0900

    Feat :sparkles: : 게임정보 렌더링 구현
    하나의 게임의 10명의 유저의 게임정보 렌더링 구현

--- client/src/components/GameInfoRender.js
+++ client/src/components/GameInfoRender.js
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import _ from 'lodash';
 import './GameInfoRender.css';
 
 function GameInfoRender(props) {
   const { gameInfo, winCount } = props;
-
   const championIcon = async () => {
-    const getIcon = await axios.get(
-      'http://ddragon.leagueoflegends.com/cdn/12.15.1/data/en_US/champion.json'
-    );
+    const getIcon = await axios.get('http://ddragon.leagueoflegends.com/cdn/12.15.1/data/en_US/champion.json');
   };
-
+  // console.log(gameInfo);
   // 전체 게임 승률
   const recentGames = (winCount / 10) * 100;
 
@@ -25,28 +23,23 @@ function GameInfoRender(props) {
     const rightNum = splitNumber[1].substring(0, decimalPoint);
     return Number(`${splitNumber[0]}.${rightNum}`).toFixed(decimalPoint);
   };
+
   // teamId 200 = 레드팀
   // teamId 100 = 블루팀
+
   return (
-    <div className='gameInfoRendering'>
-      <div className='recentGames'>최근 10게임 승률:{recentGames}%</div>
-      {gameInfo.map((item, index) => {
-        // console.log('item: ', item.info.participants[index].challenges.kda);
+    <div className="gameInfoRendering">
+      <div className="recentGames">최근 10게임 승률:{recentGames}%</div>
+      {_.map(gameInfo, (item, idx) => {
         return (
-          <div className='gameInfo' key={index}>
-            <li className='gameList'>
-              챔피언 이름: {item.info.participants[index].championName}
-              레벨: {item.info.participants[index].champLevel}
-              챔피언 ID: {item.info.participants[index].championId}
-              {/* {item.info.participants[index].kills}/
-              {item.info.participants[index].deaths}/
-              {item.info.participants[index].assists}
-              {item.info.participants[index].deaths === 0
-                ? 'Perfect'
-                : `(${getNotRoundDecimalNumber(
-                    item.info.participants[index].challenges.kda
-                  )})`} */}
-              {/* 총 딜량: {item.totalDamageDealtToChampions}
+          <div className="gameInfo" key={idx}>
+            <li className="gameList">
+              챔피언 이름: {item.championName}
+              레벨: {item.champLevel}
+              챔피언 ID: {item.championId}
+              {item.kills}/{item.deaths}/{item.assists}
+              {item.deaths === 0 ? 'Perfect' : `(${getNotRoundDecimalNumber(item.challenges.kda)})`}총 딜량:{' '}
+              {item.totalDamageDealtToChampions}
               받은 피해량: {item.totalDamageTaken}
               솔킬 횟수: {item.soloKills}
               퍼스트 블러드: {item.firstBloodKill}
@@ -62,7 +55,7 @@ function GameInfoRender(props) {
               아이탬7: {item.item6}
               {item.teamId === 200 ? '레드팀' : '블루팀'}
               라인: {item.teamPosition}
-              {item.win === true ? '승리' : '패배'} */}
+              {item.win === true ? '승리' : '패배'}
             </li>
           </div>
         );
