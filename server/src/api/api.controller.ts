7e15b01c4513af9fa272dc61397e6f8b5d04e1d8
LeeHwaRang <snow2271@gmail.com>
2023-02-13 13:56:21 +0900

    로깅 추가 및 코드 정리

--- server/src/api/api.controller.ts
+++ server/src/api/api.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get, Param, Post, Body, HttpStatus, HttpException, Patch } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { ApiService } from './api.service';
-import { Summoner } from 'src/schema/summoner.schema';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
-import { SummonerData } from './dto/summonerData.dto';
+import { MyLogger } from 'src/common/middlewares/logger.middleware';
 
 @ApiTags('riot api')
 @Controller('api')
 export class ApiController {
   constructor(private ApiService: ApiService) {}
+  private readonly myLogger = new MyLogger(ApiController.name);
 
   @Post('summoner/info')
   @ApiOperation({
@@ -15,6 +15,7 @@ export class ApiController {
     description: '소환사 이름을 사용한 해당 유저 정보 조회',
   })
   async getUser(@Body('name') name: string) {
+    this.myLogger.log(`${name}님의 정보 조회중...`);
     return this.ApiService.getUserInfo(name);
   }
 
@@ -24,6 +25,7 @@ export class ApiController {
     description: '암호화된 소환사 아이디를 사용한 해당 소환사 리그정보 조회',
   })
   async getLeague(@Body('id') id: string) {
+    this.myLogger.log(`리그정보 조회중...`);
     return this.ApiService.getLeagueInfo(id);
   }
 
@@ -33,6 +35,7 @@ export class ApiController {
     description: 'userInfo API 에서 요청받은 항목중 puuid를 사용한 게임 매칭 ID조회',
   })
   async getMatch(@Body('puuid') puuid: string) {
+    this.myLogger.log(`게임 매칭 ID조회중...`);
     return this.ApiService.getMatchId(puuid);
   }
 
@@ -42,6 +45,7 @@ export class ApiController {
     description: 'matchInfo API에서 요청한 matchId를 사용해 해당 게임 정보조회',
   })
   async getGameInfo(@Body('data') data: string[]) {
+    this.myLogger.log(`게임정보 조회중...`);
     return this.ApiService.getGameInfoForMatchId(data);
   }
 
@@ -51,6 +55,7 @@ export class ApiController {
     description: '소환사 이름으로 게임정보 조회 API',
   })
   async gameInfo(@Body('name') name: string) {
+    this.myLogger.log(`${name}의 게임정보 조회중...`);
     return this.ApiService.getGameDataForSummonerName(name);
   }
 }
