f4fbcfcb5ce65f9b3c2f56eb3ad2e15830885aca
LeeHwaRang <snow2271@gmail.com>
2023-02-13 13:55:46 +0900

    http 예외처리 추가

---
+++ server/src/common/exceptions/http-exception.filter.ts
@@ -0,0 +1,21 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    const error = exception.getResponse() as string | { error: string; statusCode: number; message: string | string[] };
+
+    response.status(status).json({
+      success: false,
+      statusCode: status,
+      path: request.url,
+      error,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
