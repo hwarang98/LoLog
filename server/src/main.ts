13309d4fd28fa33e4dad565ce5cc77ce1b4fdf43
LeeHwaRang <snow2271@gmail.com>
2023-02-13 13:55:08 +0900

    예외처리 에러메시지 추가 및 코드정리

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { MyLogger } from './common/middlewares/logger.middleware.js';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
+  const PORT = process.env.PORT;
+
   const app = await NestFactory.create(AppModule);
-  // app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -13,11 +16,25 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors();
 
   const config = new DocumentBuilder().setTitle('LoLog').setDescription('라이엇 api').setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
-  app.enableCors();
-  await app.listen(4000);
+
+  await app
+    .listen(PORT, () => {
+      Logger.log(
+        `
+      ################################################
+          🛡️  Server listening on port: ${PORT} 🛡️
+      ################################################
+      `,
+      );
+    })
+    .catch((error) => {
+      Logger.error(error);
+    });
 }
 bootstrap();
