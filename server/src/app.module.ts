024d39bd00511c6a2b59f04ead793287781e47e9
LeeHwaRang <snow2271@gmail.com>
2023-02-13 13:56:32 +0900

    개발환경 세팅

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,10 @@
-import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ApiModule } from './api/api.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import * as mongoose from 'mongoose';
-import { LoggerMiddleware } from './logger.middleware';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import mongoose from 'mongoose';
 
 @Module({
   imports: [
@@ -12,13 +12,12 @@ import { LoggerMiddleware } from './logger.middleware';
     ConfigModule.forRoot({
       envFilePath:
         process.env.NODE_ENV === 'production'
-          ? '.production.env'
-          : process.env.NODE_ENV === 'stage'
-          ? '.stage.env'
+          ? '.env.production'
+          : process.env.NODE_ENV === 'development'
+          ? '.env.development'
           : '.env',
     }),
     MongooseModule.forRoot(process.env.MONGO_URL),
-    // SummonerModule,
   ],
 
   controllers: [AppController],
@@ -26,7 +25,9 @@ import { LoggerMiddleware } from './logger.middleware';
 })
 export class AppModule implements NestModule {
   // 로깅 미들웨어 등록
+  private readonly isDev: boolean = process.env.MODE === 'dev' ? true : false;
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*');
+    consumer.apply(LoggerMiddleware).forRoutes('*'); // * = 전체 엔드포인트에 loggerMiddleware적용
+    mongoose.set('debug', this.isDev);
   }
 }
