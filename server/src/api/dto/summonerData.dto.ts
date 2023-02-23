1502ecda22382b6baf8acdd184d12960fb933a89
LeeHwaRang <snow2271@gmail.com>
2023-02-23 19:12:06 +0900

    Feat :sparkles: : gameData DTO 구현

--- server/src/api/dto/summonerData.dto.ts
+++ server/src/api/dto/summonerData.dto.ts
@@ -1,4 +1,39 @@
 export interface SummonerData {
-  summonerName: string;
+  summonerName?: string;
   summonerGameData?: object[];
+}
+
+export interface SummonerGameData {
+  gameStartDate?: string;
+  gameEndDate?: string;
+  gameDuration?: string;
+  gameType?: string;
+  summonerName?: string;
+  summonerLevel?: number;
+  teamId?: number;
+  championName?: string;
+  champLevel?: number;
+  kills?: number;
+  deaths?: number;
+  assists?: number;
+  challenges?: { kda: number; jungleMonsterKill: number };
+  minionsKill: number;
+  jungleMonsterKill: number;
+  totalCs: number;
+  item0?: number;
+  item1?: number;
+  item2?: number;
+  item3?: number;
+  item4?: number;
+  item5?: number;
+  item6?: number;
+  spell1Casts?: number;
+  spell2Casts?: number;
+  spell3Casts?: number;
+  spell4Casts?: number;
+  lane?: string;
+  role?: string;
+  pinkWard?: number;
+  team?: string;
+  win?: boolean;
 }
