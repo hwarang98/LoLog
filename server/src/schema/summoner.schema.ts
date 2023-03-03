4dc4da817c5f7d8d62b7aff2d6863ef89cdd09c7
LeeHwaRang <snow2271@gmail.com>
2023-03-03 17:39:25 +0900

    기능변화 x 파라미터 이름 변경 및 스웨거 api 설정

--- server/src/schema/summoner.schema.ts
+++ server/src/schema/summoner.schema.ts
@@ -1,14 +1,14 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
-import { Document, Mixed, ObjectExpression, ObjectId, version } from 'mongoose';
+import { Document } from 'mongoose';
 
 export type SummonerDocument = Summoner & Document;
 
 @Schema({ versionKey: false })
 export class Summoner {
   @ApiProperty({
-    example: '돌면킬',
+    example: '징징대면달라지나',
     description: '소환사 이름',
   })
   @Prop({ required: true })
