816984acd6b09936ead9d5a0cd28415a879495ce
LeeHwaRang <snow2271@gmail.com>
2023-03-22 23:32:27 +0900

    Setting 🔧 라인별 아이콘 이미지 주소 설정

---
+++ client/src/config/config.js
@@ -0,0 +1,16 @@
+export const lineIcon = {
+  TOP_ICON:
+    'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-clash/global/default/assets/images/position-selector/positions/icon-position-top-hover.png',
+
+  MIDDLE_ICON:
+    'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-clash/global/default/assets/images/position-selector/positions/icon-position-middle-hover.png',
+
+  JUNGLE_ICON:
+    'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-clash/global/default/assets/images/position-selector/positions/icon-position-jungle-hover.png',
+
+  BOTTOM_ICON:
+    'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-clash/global/default/assets/images/position-selector/positions/icon-position-bottom-hover.png',
+
+  SUPPORT_ICON:
+    'https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-clash/global/default/assets/images/position-selector/positions/icon-position-utility-hover.png',
+};
